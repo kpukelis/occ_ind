ef30da495072236980a39ef7de30fbd88ec20331
kpukelis <kelseypukelis2017@u.northwestern.edu>
2017-08-25 16:14:58 -0400

    Incorporated noflag into R script; begin Rmarkdown

--- occ_ind_graphs.R
+++ occ_ind_graphs.R
@@ -1,3 +1,4 @@
+## @knitr setup
 # Load packages
   library(foreign)
   library(ggplot2)
@@ -10,7 +11,8 @@
   xhigh <- 500
 
 # Set working directory
-  setwd(dir = paste("/Users/kbp2w/Box Sync/Adam/Occupation & Industry Stats/data/clean/Extract ", extract_num))
+  #setwd(dir = paste("/Users/kbp2w/Box Sync/Adam/Occupation & Industry Stats/data/clean/Extract ", extract_num))
+  setwd(dir = "/Users/kbp2w/Box Sync/Adam/Occupation & Industry Stats/data/clean/Extract 2")
 
 # Read Stata data into R
   ind <- read.dta(file = "ind_v12.dta")
@@ -19,25 +21,47 @@
   
 # Plot functions
   # Full density plot
-    plot_full_density <- function(data, dataname) {
-      ggplot(data = data, mapping = aes(x = num_labforce)) +
-        geom_density() +
+    plot_full_density <- function(data, dataname, includeflags) {
+      if (includeflags==TRUE) {
+        ggbase <- ggplot(data = data, mapping = aes(x = num_labforce))
+      }
+      else {
+        ggbase <- ggplot(data = data, mapping = aes(x = num_labforce_noflag))
+      }
+      ggbase +
+      geom_density() +
         ggtitle(paste(dataname,"~","Full"))
     }
   
   # Density plot with shorter x limits
-    plot_partial_density <- function(data, dataname) {
-      ggplot(data = data, mapping = aes(x = num_labforce)) +
+    plot_partial_density <- function(data, dataname, includeflags) {
+      if (includeflags==TRUE) {
+        ggbase <- ggplot(data = data, mapping = aes(x = num_labforce))
+      }
+      else {
+        ggbase <- ggplot(data = data, mapping = aes(x = num_labforce_noflag))
+      }
+      ggbase +
         geom_density() +
         xlim(xlow, xhigh) +
         ggtitle(paste(dataname,"~","xlimits:",xlow,"to",xhigh))
     }
 
 # Plot all plots
-  plot_full_density(data = ind, dataname = "ind")
-  plot_full_density(data = occ, dataname = "occ")
-  plot_full_density(data = ind_occ, dataname = "ind_occ")
+## @knitr plots_noflags
+  plot_full_density(data = ind, dataname = "ind",includeflags = FALSE)
+  plot_full_density(data = occ, dataname = "occ",includeflags = FALSE)
+  plot_full_density(data = ind_occ, dataname = "ind_occ",includeflags = FALSE)
 
-  plot_partial_density(data = ind, dataname = "ind")
-  plot_partial_density(data = occ, dataname = "occ")
-  plot_partial_density(data = ind_occ, dataname = "ind_occ")
+  plot_partial_density(data = ind, dataname = "ind", includeflags = FALSE)
+  plot_partial_density(data = occ, dataname = "occ",includeflags = FALSE)
+  plot_partial_density(data = ind_occ, dataname = "ind_occ",includeflags = FALSE)
+## @knitr plots_withflags
+  plot_full_density(data = ind, dataname = "ind",includeflags = TRUE)
+  plot_full_density(data = occ, dataname = "occ",includeflags = TRUE)
+  plot_full_density(data = ind_occ, dataname = "ind_occ",includeflags = TRUE)
+  
+  plot_partial_density(data = ind, dataname = "ind", includeflags = TRUE)
+  plot_partial_density(data = occ, dataname = "occ",includeflags = TRUE)
+  plot_partial_density(data = ind_occ, dataname = "ind_occ",includeflags = TRUE)
+  
