f1c630d7e84d37cc0c58d1ae7e5bc69752c76f74
kpukelis <kelseypukelis2017@u.northwestern.edu>
2017-08-25 18:04:01 -0400

    Completely functional for extract 3, including R markdown
    Sent to Adam 8/25/2017

--- occ_ind_graphs.R
+++ occ_ind_graphs.R
@@ -6,10 +6,6 @@
 # Set working directory
   setwd(dir = "/Users/kbp2w/Box Sync/Adam/Occupation & Industry Stats/data/clean/Extract 3")
   
-# Set graph parameters
-  xlow <- 0
-  xhigh <- 500
-
 # Read Stata data into R
   ind <- read.dta(file = "ind_v12.dta")
   occ <- read.dta(file = "occ_v12.dta")
@@ -25,12 +21,18 @@
         ggbase <- ggplot(data = data, mapping = aes(x = num_labforce_noflag))
       }
       ggbase +
-      geom_density() +
+        geom_histogram(aes(y=..density..),
+                       bins = 100,
+                       color="black", 
+                       fill="grey") +
+        geom_density(alpha=.2,
+                     color = "#000099",
+                     fill="#000099") +
         ggtitle(paste(dataname,"~","Full"))
     }
   
   # Density plot with shorter x limits
-    plot_partial_density <- function(data, dataname, includeflags) {
+    plot_partial_density <- function(data, dataname, includeflags, xhigh) {
       if (includeflags==TRUE) {
         ggbase <- ggplot(data = data, mapping = aes(x = num_labforce))
       }
@@ -38,11 +40,20 @@
         ggbase <- ggplot(data = data, mapping = aes(x = num_labforce_noflag))
       }
       ggbase +
-        geom_density() +
-        xlim(xlow, xhigh) +
-        ggtitle(paste(dataname,"~","xlimits:",xlow,"to",xhigh))
+        geom_histogram(aes(y=..density..),
+                       binwidth = xhigh/50,
+                       color="black", 
+                       fill="grey") +
+        geom_density(alpha=.2, 
+                     color = "#000099",
+                     fill="#000099") +
+        xlim(0, xhigh) +
+        ggtitle(paste(dataname,"~","xlimits:","0","to",xhigh))
     }
 
+# Data frame list
+    df.list <- list(ind,occ,ind_occ)
+    
 # Plot all plots
 
 ## @knitr plots_noflags
@@ -50,16 +61,24 @@
   plot_full_density(data = occ, dataname = "occ",includeflags = FALSE)
   plot_full_density(data = ind_occ, dataname = "ind_occ",includeflags = FALSE)
 
-  plot_partial_density(data = ind, dataname = "ind", includeflags = FALSE)
-  plot_partial_density(data = occ, dataname = "occ",includeflags = FALSE)
-  plot_partial_density(data = ind_occ, dataname = "ind_occ",includeflags = FALSE)
+  plot_partial_density(data = ind, dataname = "ind", includeflags = FALSE, xhigh = 20000)
+  plot_partial_density(data = occ, dataname = "occ",includeflags = FALSE, xhigh = 15000)
+  plot_partial_density(data = ind_occ, dataname = "ind_occ",includeflags = FALSE, xhigh = 200)
 
 ## @knitr plots_withflags
   plot_full_density(data = ind, dataname = "ind",includeflags = TRUE)
   plot_full_density(data = occ, dataname = "occ",includeflags = TRUE)
   plot_full_density(data = ind_occ, dataname = "ind_occ",includeflags = TRUE)
   
-  plot_partial_density(data = ind, dataname = "ind", includeflags = TRUE)
-  plot_partial_density(data = occ, dataname = "occ",includeflags = TRUE)
-  plot_partial_density(data = ind_occ, dataname = "ind_occ",includeflags = TRUE)
+  plot_partial_density(data = ind, dataname = "ind", includeflags = TRUE, xhigh = 20000)
+  plot_partial_density(data = occ, dataname = "occ",includeflags = TRUE, xhigh = 15000)
+  plot_partial_density(data = ind_occ, dataname = "ind_occ",includeflags = TRUE, xhigh = 200)
+  
+## @knitr stats
+  #Total # of obs (ind, occ, ind_occ)
+  lapply(df.list, function(x) nrow(x))
+  #Total # of obs with 100 workers or fewer--including flagged data (ind, occ, ind_occ)
+  lapply(df.list, function(x) nrow(x[x$num_labforce<=100,]))
+  #Total # of obs with 100 workers or fewer--excluding flagged data (ind, occ, ind_occ)
+  lapply(df.list, function(x) nrow(x[x$num_labforce_noflag<=100,]))
   
